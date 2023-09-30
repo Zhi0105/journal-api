6dcf90d5611ee6dcd2cf9c399ae189bee342af38
Argie <argie.b@flowerstore.ph>
2023-09-30 21:33:22 +0800

    main module mod

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,14 +4,6 @@ import { UserModule } from './user/user.module';
 import { CategoryModule } from './category/category.module';
 import { PrismaModule } from './prisma/prisma.module';
 
-
-//  NEST JS COMMAND :
-// nest new (project_name) --typescript
-// nest g module (module_name)
-// nest g controller (controller_name)
-// nest g service (service_name) --no-spec
-
-
 @Module({
   imports: [ AuthModule, UserModule, CategoryModule, PrismaModule ],
 })
